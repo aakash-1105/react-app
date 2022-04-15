56aec95a75bd4ff0cfccb86d964889a8d379a5b2
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-04-15 21:00:24 +0530

    Onlick event example for useRef

--- src/component/Hooks/UseRefHook.js
+++ src/component/Hooks/UseRefHook.js
@@ -4,26 +4,37 @@ const UseRefHook = () => {
     const ref = useRef()
     const refCount = useRef(0)
     const [count , setCount] = useState(0)
-    useEffect(() => {
-        ref.current.focus()
 
+    // useEffect(() => {
+    //     ref.current.focus()
+
+    //     const interval = setInterval(() => {
+    //         refCount.current = refCount.current + 1
+    //         if(refCount.current >= 10) {
+    //             clearInterval(interval)
+    //         }
+    //         setCount(prevCount => prevCount + 1)
+    //     } , 1000)
+
+    //     return (() => {
+    //         clearInterval(interval)
+    //     })
+    // } , [])
+    // console.log(count)
+
+    useEffect(() => {
         const interval = setInterval(() => {
             refCount.current = refCount.current + 1
-            if(refCount.current >= 10) {
-                clearInterval(interval)
-            }
-            setCount(prevCount => prevCount + 1)
-        } , 1000)
-
-        return (() => {
-            clearInterval(interval)
-        })
+        } , 10)
+        return (() => clearInterval(interval))
     } , [])
-    console.log(count)
+    console.log("re-render")
   return (
     <div>
         <input type="text" ref={ref}></input>
+        <h1>Count : {count}</h1>
         <h1>RefCount counting no. of renders : {refCount.current}</h1>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment Count</button>
     </div>
   )
 }
