f4f9b0d00b9e1d5990bfdaf55aeab2420eaa5ebf
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-04-15 21:17:09 +0530

    another example of useRef

--- src/component/Hooks/UseRefHook.js
+++ src/component/Hooks/UseRefHook.js
@@ -22,19 +22,27 @@ const UseRefHook = () => {
     // } , [])
     // console.log(count)
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            refCount.current = refCount.current + 1
-        } , 10)
-        return (() => clearInterval(interval))
-    } , [])
-    console.log("re-render")
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         refCount.current = refCount.current + 1
+    //     } , 10)
+    //     return (() => clearInterval(interval))
+    // } , [])
+    // console.log("re-render")
+
+    const incrementRefCount = () => {
+        refCount.current += 1
+        if(refCount.current % 5 === 0 ) setCount(prevCount => prevCount + 1)
+    }
+    console.log("Re-render count")
   return (
     <div>
         <input type="text" ref={ref}></input>
         <h1>Count : {count}</h1>
-        <h1>RefCount counting no. of renders : {refCount.current}</h1>
-        <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment Count</button>
+        {/* <h1>RefCount counting no. of renders : {refCount.current}</h1> */}
+        {/* <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment Count</button> */}
+        <h1>RefCount : {refCount.current}</h1>
+        <button onClick={incrementRefCount}>Increment RefCount</button>
     </div>
   )
 }
