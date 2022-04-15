0713a8d15b77ffe1bb15c2519d78a841a9453317
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-04-15 20:31:06 +0530

    used ref to count the no. of render and cleared the timer after 10 renders

--- src/component/Hooks/UseRefHook.js
+++ src/component/Hooks/UseRefHook.js
@@ -1,13 +1,29 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const UseRefHook = () => {
     const ref = useRef()
+    const refCount = useRef(0)
+    const [count , setCount] = useState(0)
     useEffect(() => {
         ref.current.focus()
-    } ,[])
+
+        const interval = setInterval(() => {
+            refCount.current = refCount.current + 1
+            if(refCount.current >= 10) {
+                clearInterval(interval)
+            }
+            setCount(prevCount => prevCount + 1)
+        } , 1000)
+
+        return (() => {
+            clearInterval(interval)
+        })
+    } , [])
+    console.log(count)
   return (
     <div>
         <input type="text" ref={ref}></input>
+        <h1>RefCount counting no. of renders : {refCount.current}</h1>
     </div>
   )
 }
